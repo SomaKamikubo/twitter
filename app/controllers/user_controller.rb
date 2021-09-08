275bc97245cb98477fbd75dcac80b6fb81b1598b
e195708 <e195708@ie.u-ryukyu.ac.jp>
2021-09-09 01:53:59 +0900

    新規会員登録の中盤まで

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,28 @@
+class UserController < ApplicationController
+  def new
+    @user = User.new()
+  end
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @root_url }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:name,:email,:password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'user/new'
   resources :tweets
   get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get    '/entry',   to: 'user#new'
+  post   '/user/new',   to: 'user#create'
   root :to => 'tweets#index'
 end
