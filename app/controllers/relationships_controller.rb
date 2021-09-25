feefb7b2b26ae5f387a5e25f1fc4ae6b58ebee23
shimayuki01 <yuki02129@icloud.com>
2021-09-25 16:44:30 +0900

    followバグ修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,10 +1,12 @@
 class RelationshipsController < ApplicationController
   def create
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
   end
--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,6 @@ Rails.application.routes.draw do
     get :follows, on: :member
     get :followers, on: :member
   end 
+  delete 'users/:user_id/relationships', to: 'relationships#destroy' 
+
 end
